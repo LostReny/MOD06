9cee217d97972d22d52271ad72ef682709032488
Isabelle Reny <isabelleribaldo@hotmail.com>
2023-01-19 12:03:16 -0300

    Player movement
    Moving the player right / left
    Shooting a projectile with direction and speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
+    #region Variables
+    [Header("Direction and velocity")]
+    public Vector3 dir;
+    public int speed;
 
-    // Update is called once per frame
+    [Header("GameObject")]
+    public GameObject Projectile;
+    public Transform Shotter;
+    
+    #endregion
+
+    #region Method movement input
+    // if we press the left arrow - it will move to the left | the right arrow is the same thing
+    // why it use speed and Time.delta time? - Unity time execution 
+    //speed is suppose to be a variable? - yes - we use int to make a velocity in unity inspector 
+    // GetKey - movement will be executed while pressing the button
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        else if(Input.GetKey(KeyCode.RightArrow)){
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+     #endregion   
+
+    #region Shotting
+    if(Input.GetMouseButtonDown(0)){
+        SpawnItem();
+        Debug.Log("MouseButton");
+    }  
+    #endregion
     }
+
+    #region Instantiate
+    public void SpawnItem(){
+        var obj = Instantiate(Projectile);
+        obj.transform.position = Shotter.transform.position;
+    }
+    #endregion
+
 }
